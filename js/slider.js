7c6407a13d4a79f4063347b03080aa080d6d0e9e
Zuhair Abid <zuhairabid.official@gmail.com>
2024-03-29 04:29:51 +0600

    rough

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    // Add your options here
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    // Add more options as needed
+  })
+})
